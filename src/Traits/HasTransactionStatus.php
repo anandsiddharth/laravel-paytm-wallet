ad88b6628dde25fcff381d78628cfd854efa8260
Anand Siddharth <anand_hashtaag@bitbucket.org>
2018-11-27 13:01:40 +0530

    fix std object to rray

--- src/Traits/HasTransactionStatus.php
+++ src/Traits/HasTransactionStatus.php
@@ -6,21 +6,28 @@ use Anand\LaravelPaytmWallet\Facades\PaytmWallet;
 trait HasTransactionStatus {
 
     public function isOpen(){
-        if ($this->response()->STATUS == PaytmWallet::STATUS_OPEN){
+        if ($this->response['STATUS'] == PaytmWallet::STATUS_OPEN){
             return true;
         }
         return false;
     }
 
     public function isFailed(){
-        if ($this->response()->STATUS == PaytmWallet::STATUS_FAILURE) {
+        if ($this->response['STATUS'] == PaytmWallet::STATUS_FAILURE) {
             return true;
         }
         return false;
     }
 
     public function isSuccessful(){
-        if($this->response()->STATUS == PaytmWallet::STATUS_SUCCESSFUL){
+        if($this->response['STATUS'] == PaytmWallet::STATUS_SUCCESSFUL){
+            return true;
+        }
+        return false;
+    }
+
+    public function isPending(){
+        if($this->response['STATUS'] == PaytmWallet::STATUS_PENDING){
             return true;
         }
         return false;
