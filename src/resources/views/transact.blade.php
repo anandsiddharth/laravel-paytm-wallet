52c53989be64205ae8382316b9314d5a30b68786
Anand Siddharth <anand_hashtaag@bitbucket.org>
2018-12-02 00:30:42 +0530

    use same form

--- src/resources/views/transact.blade.php
+++ src/resources/views/transact.blade.php
@@ -7,18 +7,6 @@
 	<br>
 	<center><h1>Your transaction is being processed!!!</h1></center>
 	<center><h2>Please do not refresh this page...</h2></center>
-		<form method="post" action="{{$txn_url}}" name="f1">
-		<table border="1">
-			<tbody>
-				@foreach ($params as $key => $value)
-					<input type="hidden" name="{{$key}}"  value="{{$value}}" />
-				@endforeach
-				<input type="hidden" name="CHECKSUMHASH" value="{{$checkSum}}">
-			</tbody>
-		</table>
-		<script type="text/javascript">
-			document.f1.submit();
-		</script>
-	</form>
+	@yield('payment_redirect')
 </body>
 </html>
